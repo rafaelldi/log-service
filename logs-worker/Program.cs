c2d3f43dd541f1f26df7ed192c9710006496acc2
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2022-12-29 21:06:38 +0100

    Add worker service

---
+++ logs-worker/Program.cs
@@ -0,0 +1,10 @@
+using logs_worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
